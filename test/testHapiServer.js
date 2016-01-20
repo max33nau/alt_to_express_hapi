b709ee1a6e5b3f8f722432311b58f9aa75fcbb80
max33nau <mtj29@nau.edu>
2016-01-20 12:13:21 -0800

    new validation made for params,query, and payload

--- test/testHapiServer.js
+++ test/testHapiServer.js
@@ -15,13 +15,19 @@ describe('alternative to express using hapi/mongooose mongo ', function() {
   var dbInfo = startDB();
 
 
-  before(function(done, done1) {
-    startServer(done);
-    dbInfo.start(done1);
+  before(function(cb, done) {
+    startServer(cb);
+    dbInfo.start(done);
   });
 
-  it('should put a new file in the data folder', function() {
-
+  it('should give a response where the content type is html for home page', function(done) {
+    chai.request('localhost:3030')
+      .get('/')
+      .end(function(error, response) {
+        expect(error).to.be.null;
+        expect(response).to.have.status(200);
+        done();
+      });
   });
 
 
